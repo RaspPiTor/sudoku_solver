e9f96b4e9a258cd475e9a23d947273b57702fca6
Alex Williams <hughaw64@gmail.com>
2019-03-21 22:22:25 +0000

    Optimised
    Reduced calculation of row, column and box sets by cloning

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #![feature(vec_remove_item)]
 use std::collections::HashSet;
 
+#[derive(Clone)]
 struct Solver {
-    routes: Vec<[u8; 81]>,
     rows: Vec<HashSet<u8>>,
     columns: Vec<HashSet<u8>>,
     boxes: Vec<HashSet<u8>>,
     options: HashSet<u8>,
-    solution: [u8; 81],
+    data: [u8; 81],
+    to_explore: Vec<usize>,
 }
 
 impl Solver {
@@ -28,48 +29,61 @@ impl Solver {
         for _ in 0..27 {
             boxes.push(options.clone());
         }
-        Solver {
-            routes: vec![sudoku],
+        let mut to_explore: Vec<usize> = Vec::new();
+        for i in 0..81 {
+            if { sudoku[i] == 0 } {
+                to_explore.push(i);
+            }
+        }
+        let mut solver = Solver {
             rows: rows,
             columns: columns,
             boxes: boxes,
             options: options,
-            solution: [0; 81],
+            data: sudoku,
+            to_explore: to_explore,
+        };
+        for i in 0..81 {
+            solver.generate(i);
         }
+        solver
     }
-    fn generate(&mut self, square: usize, route: &[u8; 81]) {
+    fn generate(&mut self, square: usize) {
         let row_pos: usize = square / 9;
         let column_pos: usize = square % 9;
         let box_pos: usize = square / 27 * 3 + square / 3 % 3;
         let mut row: HashSet<u8> = HashSet::with_capacity(9);
         for i in 0..9 {
-            row.insert(route[row_pos * 9 + i]);
+            row.insert(self.data[row_pos * 9 + i]);
         }
         let mut column: HashSet<u8> = HashSet::with_capacity(9);
         for i in &[0, 9, 18, 27, 36, 45, 54, 63, 72] {
-            column.insert(route[column_pos + *i]);
+            column.insert(self.data[column_pos + *i]);
         }
         let mut cbox: HashSet<u8> = HashSet::with_capacity(9);
         for i in &[0, 1, 2, 9, 10, 11, 18, 19, 20] {
-            cbox.insert(route[box_pos / 3 * 27 + box_pos % 3 * 3 + *i]);
+            cbox.insert(self.data[box_pos / 3 * 27 + box_pos % 3 * 3 + *i]);
         }
         self.rows[row_pos] = row;
         self.columns[column_pos] = column;
         self.boxes[box_pos] = cbox;
     }
-    fn process(&mut self, to_explore: &mut Vec<usize>, route: &mut [u8; 81]) -> bool {
-        for i in 0..81 {
-            self.generate(i, &route);
-        }
-        for _ in 0..to_explore.len() {
+    #[inline(never)]
+    fn process(&mut self, routes: &mut Vec<Solver>) -> bool {
+        for _ in 0..self.to_explore.len() {
             let mut min_length = 11;
             let mut min_pos = 0;
             let mut min_result: Vec<u8> = Vec::new();
-            for i in to_explore.iter() {
-                let mut excluded = self.rows[i / 9].clone();
-                excluded.extend(self.columns[i % 9].clone());
-                excluded.extend(self.boxes[i / 27 * 3 + i / 3 % 3].clone());
-                let result: Vec<&u8> = self.options.difference(&excluded).collect();
+            for i in self.to_explore.iter() {
+                let mut result: HashSet<u8> = HashSet::new();
+                let row = &self.rows[i / 9];
+                let column = &self.columns[i % 9];
+                let cbox = &self.boxes[i / 27 * 3 + i / 3 % 3];
+                for x in 1..10 {
+                    if { !(row.contains(&x) || column.contains(&x) || cbox.contains(&x)) } {
+                        result.insert(x);
+                    }
+                }
                 if { result.len() < min_length } {
                     match result.len() {
                         0 => return false,
@@ -77,7 +91,7 @@ impl Solver {
                             min_pos = *i;
                             min_result.clear();
                             for x in result {
-                                min_result.push(*x);
+                                min_result.push(x);
                             }
                             break;
                         }
@@ -86,37 +100,47 @@ impl Solver {
                             min_pos = *i;
                             min_result.clear();
                             for x in result {
-                                min_result.push(*x);
+                                min_result.push(x);
                             }
                         }
                     };
                 };
             }
-            to_explore.remove_item(&min_pos);
+            self.to_explore.remove_item(&min_pos);
             let item = min_result.pop().unwrap();
             for value in min_result.iter() {
-                route[min_pos] = *value;
-                self.routes.push(route.clone())
+                self.data[min_pos] = *value;
+                self.generate(min_pos);
+                routes.push(self.clone());
             }
-            route[min_pos] = item;
-            self.generate(min_pos, &route);
+            self.data[min_pos] = item;
+            self.generate(min_pos);
         }
 
         true
     }
+}
+
+struct SolverManager {
+    routes: Vec<Solver>,
+    solution: [u8; 81],
+}
+
+impl SolverManager {
+    pub fn new(sudoku: [u8; 81]) -> SolverManager {
+        SolverManager {
+            routes: vec![Solver::new(sudoku)],
+            solution: [0; 81],
+        }
+    }
+    #[inline(never)]
     pub fn next(&mut self) -> bool {
         if { self.routes.len() > 0 } {
             let mut route = self.routes.pop().unwrap();
-            let mut to_explore: Vec<usize> = Vec::new();
-            for i in 0..81 {
-                if { route[i] == 0 } {
-                    to_explore.push(i);
-                }
-            }
-            let result = self.process(&mut to_explore, &mut route);
+            let result = route.process(&mut self.routes);
             if { result } {
+                self.solution = route.data.clone();
                 self.routes.clear();
-                self.solution = route;
             } else {
                 return false;
             };
@@ -131,7 +155,7 @@ fn main() {
         0, 0, 0, 0, 0, 4, 4, 1, 0, 0, 0, 0, 7, 9, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 9, 0, 0,
         2, 7, 1, 7, 0, 0, 0, 0, 5, 4, 0, 6, 0, 0, 2, 0, 0, 0, 0, 0, 0,
     ];
-    let mut s = Solver::new(sudoku);
+    let mut s = SolverManager::new(sudoku);
     while { !s.next() } {}
     let mut v: Vec<u8> = Vec::new();
     for i in s.solution.iter() {
