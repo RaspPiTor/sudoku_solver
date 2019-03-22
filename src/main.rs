7f7bb1626deea19d12972f1574a3b2391b50899d
Alex Williams <hughaw64@gmail.com>
2019-03-22 18:20:14 +0000

    Added another benchmark

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,19 @@ mod tests {
             test::black_box(&s);
         });
     }
+    #[bench]
+    fn bench_8802hard(b: &mut Bencher) {
+        let sudoku: [u8; 81] = test::black_box([
+            0, 5, 0, 4, 0, 0, 9, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 5, 9, 0, 0, 0, 0, 7, 6, 3, 0, 0, 7,
+            5, 0, 0, 0, 0, 0, 4, 4, 1, 0, 0, 0, 0, 7, 9, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 9,
+            0, 0, 2, 7, 1, 7, 0, 0, 0, 0, 5, 4, 0, 6, 0, 0, 2, 0, 0, 0, 0, 0, 0,
+        ]);
+        b.iter(|| {
+            let mut s = SolverManager::new(sudoku);
+            while { !s.next() } {}
+            test::black_box(&s);
+        });
+    }
 }
 
 #[derive(Clone)]
