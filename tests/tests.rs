caadd387be7c34b2dd3c4ba7ddd8bbe74a0c6128
Alex Williams <hughaw64@gmail.com>
2019-03-25 17:44:14 +0000

    Simplified tests
    used .to_vec()

--- tests/tests.rs
+++ tests/tests.rs
@@ -14,17 +14,13 @@ mod tests {
             0, 5, 0, 7, 0, 0, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0,
             0, 0, 0, 7, 3, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 9,
         ];
-        let result: [u8; 81] = sudoku_solver::solve(sudoku);
-        let mut v: Vec<u8> = Vec::with_capacity(81);
-        for i in result.iter() {
-            v.push(*i);
-        }
+        let result: Vec<u8> = sudoku_solver::solve(sudoku).to_vec();
         let solution: Vec<u8> = vec![
             9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 4, 6, 1, 7, 3, 9, 8, 5, 3, 5, 1, 9, 2, 8, 7, 4, 6, 1, 2,
             8, 5, 3, 7, 6, 9, 4, 6, 3, 4, 8, 9, 2, 1, 5, 7, 7, 9, 5, 4, 6, 1, 8, 3, 2, 5, 1, 9, 2,
             8, 6, 4, 7, 3, 4, 7, 2, 3, 1, 9, 5, 6, 8, 8, 6, 3, 7, 4, 5, 2, 1, 9,
         ];
-        assert_eq!(v, solution);
+        assert_eq!(result, solution);
     }
 
     #[test]
@@ -34,17 +30,13 @@ mod tests {
             0, 5, 0, 7, 0, 0, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0,
             0, 0, 0, 7, 3, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 9,
         ];
-        let result: [u8; 81] = sudoku_solver::solve(sudoku);
-        let mut v: Vec<u8> = Vec::with_capacity(81);
-        for i in result.iter() {
-            v.push(*i);
-        }
+        let result: Vec<u8> = sudoku_solver::solve(sudoku).to_vec();
         let solution: Vec<u8> = vec![
             9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 4, 6, 1, 7, 3, 9, 8, 5, 3, 5, 1, 9, 2, 8, 7, 4, 6, 1, 2,
             8, 5, 3, 7, 6, 9, 4, 6, 3, 4, 8, 9, 2, 1, 5, 7, 7, 9, 5, 4, 6, 1, 8, 3, 2, 5, 1, 9, 2,
             8, 6, 4, 7, 3, 4, 7, 2, 3, 1, 9, 5, 6, 8, 8, 6, 3, 7, 4, 5, 2, 1, 9,
         ];
-        assert_eq!(v, solution);
+        assert_eq!(result, solution);
     }
     #[test]
     fn random17_test() {
@@ -53,16 +45,12 @@ mod tests {
             0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 5, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 1, 8, 0, 0, 0, 0,
             8, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 5, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0,
         ];
-        let result: [u8; 81] = sudoku_solver::solve(sudoku);
-        let mut v: Vec<u8> = Vec::with_capacity(81);
-        for i in result.iter() {
-            v.push(*i);
-        }
+        let result: Vec<u8> = sudoku_solver::solve(sudoku).to_vec();
         let solution: Vec<u8> = vec![
             2, 6, 4, 7, 1, 5, 8, 3, 9, 1, 3, 7, 8, 9, 2, 6, 4, 5, 5, 9, 8, 4, 3, 6, 2, 7, 1, 4, 2,
             3, 1, 7, 8, 5, 9, 6, 8, 1, 6, 5, 4, 9, 7, 2, 3, 7, 5, 9, 6, 2, 3, 4, 1, 8, 3, 7, 5, 2,
             8, 1, 9, 6, 4, 9, 8, 2, 3, 6, 4, 1, 5, 7, 6, 4, 1, 9, 5, 7, 3, 8, 2,
         ];
-        assert_eq!(v, solution);
+        assert_eq!(result, solution);
     }
 }
