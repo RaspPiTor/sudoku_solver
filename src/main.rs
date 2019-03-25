bc9455aff11a4b4e7fdcd193fc48403bf000c099
Alex Williams <hughaw64@gmail.com>
2019-03-25 17:53:18 +0000

    Added file support
    Doesn't yet output solutions though

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,68 @@
+extern crate clap;
+use clap::{App, Arg};
+use std::io::BufRead;
+
 use sudoku_solver;
 
 fn main() {
-    let sudoku: [u8; 81] = [
-        0, 5, 0, 4, 0, 0, 9, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 5, 9, 0, 0, 0, 0, 7, 6, 3, 0, 0, 7, 5,
-        0, 0, 0, 0, 0, 4, 4, 1, 0, 0, 0, 0, 7, 9, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 9, 0, 0,
-        2, 7, 1, 7, 0, 0, 0, 0, 5, 4, 0, 6, 0, 0, 2, 0, 0, 0, 0, 0, 0,
-    ];
-    let result: [u8; 81] = sudoku_solver::solve(sudoku);
-    let mut v: Vec<u8> = Vec::with_capacity(81);
-    for i in result.iter() {
-        v.push(*i);
+    let matches = App::new("Sudoku Solver")
+        .version("0.1.0")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("File containig sudoku's")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+    let file =
+        std::fs::File::open(matches.value_of("INPUT").unwrap()).expect("Failed to open file");
+    let lines = std::io::BufReader::new(file).lines();
+    let mut sudokus: Vec<[u8; 81]> = Vec::new();
+    for line in lines {
+        let line = match line {
+            Ok(l) => l,
+            Err(_) => {
+                println!("Encountered error");
+                break;
+            }
+        };
+        if line.len() != 81 {
+            println!("Wrong line length: {:?}", line);
+            continue;
+        }
+        let mut sudoku: [u8; 81] = [0; 81];
+        for i in 0..81 {
+            let value = match line.chars().nth(i) {
+                Some(v) => match v {
+                    '0' => 0,
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                    v => {
+                        println!("Invalid char:{:?} in line: {:?}", v, line);
+                        break;
+                    }
+                },
+                None => {
+                    println!("Error");
+                    break;
+                }
+            };
+            sudoku[i] = value;
+        }
+        sudokus.push(sudoku);
     }
-    println!("Solution:{:?}", v);
+    println!("Processed file");
+    println!("Found {:?} valid sudokus", sudokus.len());
+    let mut solutions: Vec<[u8; 81]> = Vec::with_capacity(sudokus.len());
+    for sudoku in &sudokus {
+        solutions.push(sudoku_solver::solve(*sudoku));
+    }
 }
